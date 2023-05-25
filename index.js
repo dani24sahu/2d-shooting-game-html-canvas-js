c50bc881aef6a5f82a7eeb6e57a66c07a2e56f1a
Dani <daniramsahu2001@gmail.com.address>
2023-05-25 22:25:05 +0530

    Created Explosion effect and Heavy Weapon

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ canvas.height = innerHeight;
 
 // Creating Canvas Context
 const context = canvas.getContext("2d");
-
+const lightWeaponDamage = 10;
+const heavyWeaponDamage = 30;
 let difficulty = 2;
 const form = document.querySelector("form");
 const scoreBoard = document.querySelector(".scoreBoard");
@@ -76,12 +77,13 @@ class Player {
 //--------------------------------
 // Creating Weapon Class
 class Weapon {
-  constructor(x, y, radius, color, velocity) {
+  constructor(x, y, radius, color, velocity, damage) {
     this.x = x;
     this.y = y;
     this.radius = radius;
     this.color = color;
     this.velocity = velocity;
+    this.damage = damage;
   }
 
   // Creating an Arc and drawing it with draw()
@@ -139,14 +141,55 @@ class Enemy {
   }
 }
 
+// Creating Particle Class
+const friction = 0.99;
+class Particle {
+  constructor(x, y, radius, color, velocity) {
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+    this.color = color;
+    this.velocity = velocity;
+    this.alpha = 1;
+  }
+
+  // Creating an Arc and drawing it with draw()
+  draw() {
+    context.save();
+    context.globalAlpha = this.alpha;
+    context.beginPath();
+    context.arc(
+      this.x,
+      this.y,
+      this.radius,
+      (Math.PI / 180) * 0,
+      (Math.PI / 180) * 360,
+      false
+    );
+    context.fillStyle = this.color;
+    context.fill();
+    context.restore();
+  }
+
+  update() {
+    this.draw();
+    this.velocity.x *= friction;
+    this.velocity.y *= friction;
+    this.x += this.velocity.x;
+    this.y += this.velocity.y;
+    this.alpha -= 0.01;
+  }
+}
+
 //---------------------------------------Main Logic---------------------------------------------
 // Creating Player Object, weapons array, enemiies array, and others
 const dani = new Player(playerPosition.x, playerPosition.y, 15, "white");
-
 // Weapons array
 const weapons = [];
 // Enemy Array
 const enemies = [];
+// Particle Array
+const particles = [];
 
 // -----------------Function to Spawn Enemy at Random location-------------------
 const spawnEnemy = () => {
@@ -201,10 +244,20 @@ function animation() {
   context.fillRect(0, 0, canvas.width, canvas.height);
   // Drawing player
   dani.draw();
+
+  // Generating Particles
+  particles.forEach((particle, particleIndex) => {
+    if (particle.alpha <= 0) {
+      particles.splice(particleIndex, 1);
+    } else {
+      particle.update();
+    }
+  });
+
   // Generating bullets
   weapons.forEach((weapon, weaponIndex) => {
     weapon.update();
-
+    //Remmoving weapons if they are off screen
     if (
       weapon.x + weapon.radius < 1 ||
       weapon.y + weapon.radius < 1 ||
@@ -218,30 +271,44 @@ function animation() {
   enemies.forEach((enemy, enemyIndex) => {
     enemy.update();
 
+    //  Finding distance between Player and Enemy
     const distanceBetweenPlayerAndEnemy = Math.hypot(
       dani.x - enemy.x,
       dani.y - enemy.y
     );
 
+    // Stopping game if Enemy hits Player
     if (distanceBetweenPlayerAndEnemy - dani.radius - enemy.radius < 1) {
       cancelAnimationFrame(animationId);
     }
 
     weapons.forEach((weapon, weaponIndex) => {
+      //  Finding distance between Weapon and Enemy
       const distanceBetweenWeaponAndEnemy = Math.hypot(
         weapon.x - enemy.x,
         weapon.y - enemy.y
       );
-      // Here adding enemy killing transitions
+      // Here adding enemy killing transitions with gsap plugin
       if (distanceBetweenWeaponAndEnemy - weapon.radius - enemy.radius < 1) {
-        if (enemy.radius > 18) {
+        // Reducing size of Enemy on hit
+        if (enemy.radius > weapon.damage + 5) {
           gsap.to(enemy, {
-            radius: enemy.radius - 10,
+            radius: enemy.radius - weapon.damage,
           });
           setTimeout(() => {
             weapons.splice(weaponIndex, 1);
           }, 0);
-        } else {
+        }
+        // Removing Enemy on hit if they are below 18
+        else {
+          for (let i = 0; i < enemy.radius * 5; i++) {
+            particles.push(
+              new Particle(weapon.x, weapon.y, Math.random() * 2, enemy.color, {
+                x: (Math.random() - 0.5) * (Math.random() * 7),
+                y: (Math.random() - 0.5) * (Math.random() * 7),
+              })
+            );
+          }
           setTimeout(() => {
             enemies.splice(enemyIndex, 1);
             weapons.splice(weaponIndex, 1);
@@ -269,7 +336,41 @@ canvas.addEventListener("click", (e) => {
   };
   // Adding light weapon in weapons array
   weapons.push(
-    new Weapon(canvas.width / 2, canvas.height / 2, 6, "white", velocity)
+    new Weapon(
+      canvas.width / 2,
+      canvas.height / 2,
+      6,
+      "white",
+      velocity,
+      lightWeaponDamage
+    )
+  );
+});
+
+// Event listener for Heavy weapon(Right click)
+canvas.addEventListener("contextmenu", (e) => {
+  e.preventDefault();
+  // finding angle with A-tan-2 method
+  // angle between player position(center) and click co-ordinates
+  const myAngle = Math.atan2(
+    e.clientY - canvas.height / 2,
+    e.clientX - canvas.width / 2
+  );
+  // Making variable for light weapon
+  const velocity = {
+    x: Math.cos(myAngle) * 3,
+    y: Math.sin(myAngle) * 3,
+  };
+  // Adding light weapon in weapons array
+  weapons.push(
+    new Weapon(
+      canvas.width / 2,
+      canvas.height / 2,
+      18,
+      "cyan",
+      velocity,
+      heavyWeaponDamage
+    )
   );
 });
 
